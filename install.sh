3403526b98a5e13e114f5b1b5f6786d422f44e31
SafwanGanz <67726326+SafwanGanz@users.noreply.github.com>
2025-02-28 01:08:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-echo -e "${CYAN}Starting installation for Youtube-Dl Downloader...${NC}"
+echo -e "${CYAN}Starting installation for InstaTubeDL...${NC}"
 
 echo -e "${CYAN}Requesting storage permissions...${NC}"
 termux-setup-storage || { echo -e "${RED}Failed to setup storage permissions${NC}"; exit 1; }
@@ -22,19 +22,25 @@ if ! command_exists node; then
     echo -e "${CYAN}Installing Node.js...${NC}"
     pkg install -y nodejs || { echo -e "${RED}Failed to install Node.js${NC}"; exit 1; }
 else
-    echo -e "${GREEN}Node.js is already installed.$(node -v)${NC}"
+    echo -e "${GREEN}Node.js is already installed. $(node -v)${NC}"
 fi
 
 if ! command_exists npm; then
     echo -e "${CYAN}Installing npm...${NC}"
     pkg install -y npm || { echo -e "${RED}Failed to install npm${NC}"; exit 1; }
 else
-    echo -e "${GREEN}npm is already installed.$(npm -v)${NC}"
+    echo -e "${GREEN}npm is already installed. $(npm -v)${NC}"
+fi
+
+if ! command_exists python; then
+    echo -e "${CYAN}Installing Python...${NC}"
+    pkg install -y python || { echo -e "${RED}Failed to install Python${NC}"; exit 1; }
+else
+    echo -e "${GREEN}Python is already installed. $(python3 -V)${NC}"
 fi
 
 if ! command_exists yt-dlp; then
     echo -e "${CYAN}Installing yt-dlp...${NC}"
-    pkg install -y python
     pip install -U yt-dlp || { echo -e "${RED}Failed to install yt-dlp${NC}"; exit 1; }
 else
     echo -e "${GREEN}yt-dlp is already installed.${NC}"
@@ -47,10 +53,25 @@ else
     echo -e "${GREEN}ffmpeg is already installed.${NC}"
 fi
 
+if ! command_exists rustc; then
+    echo -e "${CYAN}Installing Rust (required for spotdl)...${NC}"
+    pkg install -y rust || { echo -e "${RED}Failed to install Rust${NC}"; exit 1; }
+    export CARGO_BUILD_TARGET=aarch64-linux-android
+else
+    echo -e "${GREEN}Rust is already installed. $(rustc --version)${NC}"
+fi
+
+if ! command_exists spotdl; then
+    echo -e "${CYAN}Installing spotdl...${NC}"
+    pip install spotdl || { echo -e "${RED}Failed to install spotdl${NC}"; exit 1; }
+else
+    echo -e "${GREEN}spotdl is already installed.${NC}"
+fi
+
 echo -e "${CYAN}Installing Node.js dependencies...${NC}"
-npm install || { echo -e "${RED}Failed to install ${NC}"; exit 1; }
+npm install || { echo -e "${RED}Failed to install Node.js dependencies${NC}"; exit 1; }
 
 echo -e "${GREEN}Installation completed successfully!${NC}"
-echo -e "${CYAN}Run 'npm start' to start the Youtube-Dl Downloader.${NC}"
+echo -e "${CYAN}Run 'npm start' to start InstaTubeDL.${NC}"
 
 npm start
